b05bb3c19649afb37963b0b4aeeeb47062971152
hey880 <hey880@naver.com>
2021-01-17 03:19:42 +0900

    context_practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Navbar, Nav, NavDropdown, Button, Jumbotron } from "react-bootstrap";
 import "./App.css";
 import Data from "./data.js";
 import { Route, Link, Switch } from "react-router-dom";
 import Detail from './Detail';
+import axios from 'axios';
+
+//1. context 만들기 (React.createContext()로 범위생성, 이 범위의 이름은 재고context) 다른 파일에서 쓰려면 이 범위를 export할 수 있고 이걸 쓰려는 다른 파일에서
+//import {재고context} from './App.js'; 이렇게 import해서 쓸 수 있음
+export let 재고context = React.createContext();
 
 function App() {
+
+  let [재고, 재고변경] = useState([10,11,12]);
 
   let [shoes, shoes변경] = useState(Data);
 
@@ -57,23 +64,42 @@ function App() {
               <Button variant="primary">Learn more</Button>
             </p>
           </Jumbotron>
+
           <div className="container">
+
+            {/* //2. 같은 값을 공유할 HTML들을 <범위.Provider>로 싸매기. value={공유하고 싶은 값}을 적음 */}
+            <재고context.Provider value={재고}>
+
             <div className="row">
-              {
-                //a는 가져올 shoes라는 array안에 있는 데이터 하나하나를 의미
-                //i는 반복될 숫자 0,1,2
+              { 
                 shoes.map((a, i) => {
                   return <Card shoes={shoes[i]} i={i} key={i} />;
-                  //return <Card shoes{a}/> 이것도 위와 결과가 같다.
+                 
                 })
               }
-            </div>
+
+            </div>  
+          </재고context.Provider>
+          
           </div>
+
+          <button className="btn btn-primary" onClick={()=>{
+          axios.get('https://codingapple1.github.io/shop/data2.json')
+          .then((result)=>{
+            shoes변경([...shoes, ...result.data])
+          })
+          .catch(()=>{ })
+        }}>더보기</button>
+
         </div>
       </Route>
 
       <Route path="/detail/:id">
-        <Detail shoes={shoes}/>
+
+        <재고context.Provider value={재고}>
+        <Detail shoes={shoes} 재고={재고} 재고변경={재고변경}/>
+        </재고context.Provider>
+
       </Route>
 
       <Route path="/:id"> 
@@ -81,11 +107,17 @@ function App() {
       </Route>
     </Switch>    
 
+      
     </div>
   );
 }
 
+//3. 얘는 이제 공유된 재고라는 state를 마음대로 갖다 쓸 수 있음
 function Card(props) {
+
+  let 재고 = useContext(재고context);
+  //useContext(범위이름)로 공유된 값 사용 가능
+
   return (
     <div className="col-md-4">
       <img
@@ -96,8 +128,15 @@ function Card(props) {
       <p>
         {props.shoes.content} &{props.shoes.price}
       </p>
+      {재고[props.i]}
+      <Test></Test>
     </div>
   );
+}
+
+function Test(){
+  let 재고 = useContext(재고context);
+  return <p>{재고}</p>
 }
 
 export default App;
--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,8 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect, useContext} from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
+import {재고context} from './App.js';
 
 let 박스 = styled.div`
   padding : 20px;
@@ -17,14 +18,12 @@ function Detail(props){
  
   const [alert, setAlert] = useState(true);
   //true 일 때만 원하는 코드 보이게 하기 위한 상태 저장
+  let 재고 = useContext(재고context);
 
     useEffect(()=>{
      let 타이머 = setTimeout(()=>{setAlert(false)},2000); 
-     return ()=>{ clearTimeout(타이머)} //컴포넌트가 사라질 때 타이머 해제시켜서 버그 방지
-    }, [alert]) //alert라는 state가 update 될 때만 실행해달라는 조건을 붙임.
-    //이런 조건을 붙이지 않으면 해당 컴포넌트가 update 될 때마다 계속 재렌더링되어 실행됨.
-    //빈 대괄호만 적고 대괄호 안에 아무것도 넣지 않으면 컴포넌트가 동작할 때 딱 한 번만 실행되고
-    //다시는 실행되지 않음.
+     return ()=>{ clearTimeout(타이머)} 
+    }, [alert]) 
 
     let {id} = useParams();
     let history = useHistory();
@@ -35,8 +34,7 @@ function Detail(props){
             <박스>
               <제목 className="red">Detail</제목>
             </박스>
-          
-          {/*삼항연산자로 true일 경우 코드를 넣어주고 아닐경우 null을 넣어줌*/}
+            
             {
               alert === true ? (  <div className="my-alert">
               <p>재고가 얼마 남지 않았습니다</p>
@@ -55,7 +53,9 @@ function Detail(props){
                 <h4 className="pt-5">{prd.title}</h4>
                 <p>{prd.content}</p>
                 <p>{prd.price}원</p>
-                <button className="btn btn-danger">주문하기</button>
+                <div> <Info 재고={props.재고}></Info> </div>
+                <button className="btn btn-danger"
+                onClick={()=>{props.재고변경([9,10,11])}}>주문하기</button>
                 <button className="btn btn-danger" onClick={()=>{
                   // history.goBack();
                   history.push('/'); 
@@ -64,6 +64,12 @@ function Detail(props){
             </div>
           </div>
     )
+}
+
+function Info(props){
+  return (
+    <p>재고 : {props.재고[0]}</p>
+  )
 }
 
 export default Detail;
